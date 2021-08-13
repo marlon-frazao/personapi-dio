d189b24c9de0c47e7b85770cc0a85e08d0729389
marlon-frazao <frazao.marlon@gmail.com>
2021-08-13 11:39:05 -0300

    Project created

---
+++ src/test/java/com/digitalinnovationone/personapidio/PersonapiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.personapidio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
